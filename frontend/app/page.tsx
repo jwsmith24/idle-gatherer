f51d441da32a3ec7b0cd8af7a09dd205638d02fe
jake <jsmith181997@gmail.com>
2025-11-13 19:44:10 -0600

    update banner color

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <div className={"bg-orange-500 w-full p-2 rounded-2xl flex gap-2"}>
+        <div className={"bg-blue-500 w-full p-2 rounded-2xl flex gap-2"}>
           <label htmlFor="banner-cb">
             {visibility === "visible"
               ? "Banner is visible"
